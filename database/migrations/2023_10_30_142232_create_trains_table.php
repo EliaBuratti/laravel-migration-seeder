77332d619e3db91e566e0e4550617f6fb45a8dd0
EliaBuratti <elia.buratti.dev@gmail.com>
2023-10-30 15:23:49 +0100

    create database and table trains

---
+++ database/migrations/2023_10_30_142232_create_trains_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('society', 15);
+            $table->string('station_departure', 50);
+            $table->string('station_arrive', 50);
+            $table->time('time_departure');
+            $table->time('time_arrive');
+            $table->string('code', 10);
+            $table->smallInteger('carriages')->unsigned();
+            $table->boolean('on_time');
+            $table->boolean('deleted');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trains');
+    }
+};
---
+++ database/seeders/TrainTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Train;
+use Faker\Generator as Faker;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TrainTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        //
+        //$train = new Train();
+        //$train->society =
+    }
+}
